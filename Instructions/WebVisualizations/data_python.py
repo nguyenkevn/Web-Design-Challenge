a3eb60d00b89d9d91478e73b5b4aeeacf649076e
KevinN <66852096+nguyenkevn@users.noreply.github.com>
2020-09-15 17:52:51 -0400

    Small changes

---
+++ Instructions/WebVisualizations/data_python.py
@@ -0,0 +1,6 @@
+import pandas as pd
+#Import CSV into Pandas
+data = pd.read_csv("Resources/cities.csv")
+
+#Save to html source: https://stackoverflow.com/questions/14897833/save-pandas-to-html-as-a-file
+data.to_html("data.html")
